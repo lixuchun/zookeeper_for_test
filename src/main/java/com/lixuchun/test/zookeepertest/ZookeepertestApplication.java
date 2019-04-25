f2073b00cd6f6511d451efe98e3c6f34a049de27
lixuchun <lixuchun101022@163.com>
2019-04-25 21:35:34 +0800

    first commit with push

---
+++ src/main/java/com/lixuchun/test/zookeepertest/ZookeepertestApplication.java
@@ -0,0 +1,13 @@
+package com.lixuchun.test.zookeepertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeepertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeepertestApplication.class, args);
+	}
+
+}
