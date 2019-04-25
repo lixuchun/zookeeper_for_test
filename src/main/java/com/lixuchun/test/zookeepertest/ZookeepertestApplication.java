f8e8175c317b28eea383ae8bf37a5894ec4e41e6
lixuchun <1048320921@qq.com>
2019-04-25 21:53:38 +0800

    intjack
    444

--- src/main/java/com/lixuchun/test/zookeepertest/ZookeepertestApplication.java
+++ src/main/java/com/lixuchun/test/zookeepertest/ZookeepertestApplication.java
@@ -9,6 +9,7 @@ public class ZookeepertestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZookeepertestApplication.class, args);
                 int a = 0;
+		int jack = 444;
 	}
 
 }
