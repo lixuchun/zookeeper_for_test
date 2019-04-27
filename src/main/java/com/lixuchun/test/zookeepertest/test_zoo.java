a55ac5f7db1ee93098d93372e4dfd6fafd972ae7
lixuchun <1048320921@qq.com>
2019-04-27 12:34:18 +0800

    Update test_zoo.java

--- src/main/java/com/lixuchun/test/zookeepertest/test_zoo.java
+++ src/main/java/com/lixuchun/test/zookeepertest/test_zoo.java
@@ -1,6 +1,9 @@
 package com.lixuchun.test.zookeepertest;
 
 public class test_zoo {
+    public static void main () {
+        String wat = "sss";
+    }
     private int a = 66;
     private String ax = "xxx";
     private String b = "aaaa";
