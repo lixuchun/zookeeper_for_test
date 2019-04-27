b9c8b11c490de9638c0c41b0c1ccf62427337eeb
lixuchun <lixuchun101022@163.com>
2019-04-27 12:31:15 +0800

    first commit with push

--- src/main/java/com/lixuchun/test/zookeepertest/test_zoo.java
+++ src/main/java/com/lixuchun/test/zookeepertest/test_zoo.java
@@ -1,6 +1,7 @@
 package com.lixuchun.test.zookeepertest;
 
 public class test_zoo {
+    private int a = 66;
     private String ax = "xxx";
     private String b = "aaaa";
     public static void main (String args[]) {
